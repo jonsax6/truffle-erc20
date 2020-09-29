0dbafd357017380a0f72a5ee06289a4f89322b93
jonsax6 <jdc.saxjunky@gmail.com>
2020-09-29 16:47:53 -0400

    homework revisions

---
+++ migrations/2_ERC20_deploy.js
@@ -0,0 +1,15 @@
+const ERC20 = artifacts.require("ERC20");
+
+const TOKEN_NAME = "JDC_COIN"
+const TOKEN_SYMBOL = "JDCC"
+
+module.exports = async (deployer, network, accounts) => {
+  await deployer.deploy(ERC20, TOKEN_NAME, TOKEN_SYMBOL);
+  
+  const instance = await ERC20.deployed();
+  await instance.mint(accounts[1], 100);
+
+  const balance = await instance.balanceOf(accounts[1]);
+  console.log(`${balance} ${TOKEN_SYMBOL} minted to: ${accounts[1]}`)
+  
+};
--- migrations/2_erc20_deploy.js
+++
@@ -1,9 +0,0 @@
-const Migrations = artifacts.require("erc20");
-
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(erc20).then(function(instance){
-    instance.mint(accounts[1], 100).then(function(){
-      console.log("100 Ether Minted to: " + accounts[1])
-    });
-  });
-};
